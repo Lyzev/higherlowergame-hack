6f9be0e1e253406210e2c93324c325d0f213aa50
Lyzev <lyzev.info@gmail.com>
2022-03-22 19:36:23 +0100

    Improvement of code

--- higherlowergame.js
+++ higherlowergame.js
@@ -10,8 +10,17 @@
 // ==/UserScript==
 
 (function() {
-    const delay = 5000
-    let running = false
+    const delay = 5000;
+    let running = false;
+    const request = new XMLHttpRequest();
+    request.open("GET", 'http://www.higherlowergame.com/questions/get/general', false);
+    request.setRequestHeader('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36');
+    request.send();
+    const data = JSON.parse(request.responseText);
+    const answers = new Map();
+    for (let i = 0; i < data.length; i++) {
+        answers.set(data[i].keyword.toUpperCase(), data[i].searchVolume);
+    }
     setInterval(() => {
         if (!running) {
             if (document.getElementsByClassName('term-volume__volume').length !== 0) {
@@ -20,39 +29,28 @@
         }
     }, 500)
     function start() {
-        running = true
+        running = true;
         setTimeout(() => {
-            const request = new XMLHttpRequest();
-            request.open("GET", 'http://www.higherlowergame.com/questions/get/general', false);
-            request.setRequestHeader('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36')
-            request.send();
-            const data = JSON.parse(request.responseText)
             if (data != null) {
                 let play = setInterval(() => {
                     if (document.getElementsByClassName('term-volume__volume').length !== 0) {
-                        let a = document.getElementsByClassName('term-keyword__keyword')[0].innerText
-                        let one = search(data, a.substring(1, a.length - 1))
-                        let b = document.getElementsByClassName('term-keyword__keyword')[1].innerText
-                        let two = search(data, b.substring(1, b.length - 1))
+                        let a = document.getElementsByClassName('term-keyword__keyword')[0].innerText;
+                        let one = answers.get(a.substring(1, a.length - 1).toUpperCase());
+                        let b = document.getElementsByClassName('term-keyword__keyword')[1].innerText;
+                        let two = answers.get(b.substring(1, b.length - 1).toUpperCase());
                         if (two > one) {
-                            document.getElementsByClassName('term-actions__button--higher')[0].click()
+                            document.getElementsByClassName('term-actions__button--higher')[0].click();
                         } else {
-                            document.getElementsByClassName('term-actions__button--lower')[0].click()
+                            document.getElementsByClassName('term-actions__button--lower')[0].click();
                         }
                     } else {
-                        clearInterval(play)
-                        running = false
+                        clearInterval(play);
+                        running = false;
                     }
-                }, delay)
+                }, delay);
+            } else {
+                running = false;
             }
-        }, 2000)
+        }, 2000);
     }
-    function search(data, a) {
-        for (let i = 0; i < data.length; i++) {
-            if (a.toUpperCase() === data[i].keyword.toUpperCase()) {
-                return data[i].searchVolume
-            }
-        }
-        return -1
-    }
-})()
+})();
